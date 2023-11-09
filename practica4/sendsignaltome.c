07244f12bd62d95b0c918144b473a0e1814a9449
antoniocenlaces <antoniocenlaces@gmail.com>
2023-11-09 19:24:07 +0000

    ejercicio de inicio

--- practica4/sendsignaltome.c
+++ practica4/sendsignaltome.c
@@ -1,15 +1,26 @@
 /*sendsignaltome.c Práctica 4 SOPER*/
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
 
-
+void fcaptura(int s);
 int main(int argc, char *argv[]){
-int pid = getpid();
-if (argc<2) {
-    printf("Formato: ./sendsignaltome int");
+void (* syshandler)(int);
+    if (argc<2) {
+        printf("Formato: ./sendsignaltome int\n");
 
-} else {
-    kill(0,atoi(argv[1]));
+    } else {
+        int pid = getpid();
+        int senyal = atoi(argv[1]);
+        if (senyal != 9 && senyal != 23) {syshandler=signal(senyal, fcaptura);}
+        kill(pid,senyal);
+    }
 }
+
+void fcaptura(int s) {
+    void (* syshandler)(int);
+    printf("Capturada señal: %d\n", s);
+    syshandler=signal(s, fcaptura);
 }
