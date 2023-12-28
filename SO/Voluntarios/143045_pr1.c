5403c2d244c157a9efafa697514f8c82ddc36113
antoniocenlaces <antoniocenlaces@gmail.com>
2023-12-28 20:24:46 +0000

    Ejercicio voluntario 1 entregado

--- SO/Voluntarios/143045_pr1.c
+++ SO/Voluntarios/143045_pr1.c
@@ -1,5 +1,6 @@
 /* 143045_pr1.c
  * Ejercicio voluntario 1.
+ * Autor: Antonio José González Almela (NIP 143045)
  * Uso: 143045_pr1 <n> <m>
  * int n: número de procesos a lanzar
  * int m: número de caracteres que mostrará cada uno de los n procesos
@@ -12,6 +13,7 @@
 #include "error.h"
 
 // Pre: i es un entero que representa el número de proceso
+//      i <= 62
 //      m es el número de caracteres a mostrar por salida estandard
 // Post: printCaracteres muestra por salida estandar el caracter ('A' + i) m veces
 void printCaracteres(int i, int m);
@@ -22,18 +24,12 @@ int main(int argc, char *argv[]) {
             exit(1);
       }
       int i, pid;
-      i = i + 'A';
-      printf("Caracter calculado: %c\n",i);
-      char c = (char)i;
-      write(1,&c,1);
-      c='\n';
-      write(1,&c,1);
-
+      // Extracción de los argumentos y conversión a int
       int n = atoi(argv[1]); // Número de procesos que se lanzarán
       int m = atoi(argv[2]); // Número de caracteres que mostrará cada uno de los n procesos
-      // Comprobación si n <= 61 (número de caracteres diferentes a mostrar, igual
-      // alnúmero de procesos a lanzar)
-      if (n < 62){
+      // Comprobación si n <= 62 (número de caracteres diferentes a mostrar, igual
+      // al número de procesos a lanzar)
+      if (n < 63){
             // Proceso padre será el último en hacer su trabajo (proceso n)
             // Bucle para lanzar (n-1) procesos
             for (i=0; i < (n - 1); i++) {
@@ -46,13 +42,19 @@ int main(int argc, char *argv[]) {
                               exit(0);
                   }
             }
+            // Proceso padre hace la tarea del proceso n
+            printCaracteres(n - 1, m);
+            // Una vez realizada su tarea espera que acaben todos sus descendientes
+            for (i=0; i < (n - 1); i++) pid = wait(NULL);
+            // Por último un salto de línea antes de acabar
+            printf("\n");
       } else {
-            printf("\nMáximo número de procesos a lanzar: 61. Vuelve a intentarlo.\n");
+            printf("\nMáximo número de procesos a lanzar: 62. Vuelve a intentarlo.\n");
       }
 }
 
 // Pre: i es un entero que representa el número de proceso
-//      i <= 61
+//      i <= 62
 //      m es el número de caracteres a mostrar por salida estandard
 // Post: printCaracteres muestra por salida estandar el caracter ('A' + i) m veces
 void printCaracteres(int i, int m){
