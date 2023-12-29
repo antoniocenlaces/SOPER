d6a4cf4f9af509ddbeded76a8914e35bf4c34d1a
antoniocenlaces <antoniocenlaces@gmail.com>
2023-12-29 18:36:31 +0100

    algo en seniales

---
+++ SO/seniales/sigsuspend.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include "error.h"
+
+void captura(int);
+
+int main(){
+	int i;
+	sigset_t mimask,oldmask;
+	struct sigaction miaction;
+
+	sigemptyset(&mimask);
+	sigaddset(&mimask,SIGALRM);
+
+	miaction.sa_handler=captura;
+	sigemptyset(&miaction.sa_mask);
+	i=sigaction(SIGALRM,&miaction,NULL);	/* capturar señal SIGALRM */
+	if (i==-1) {syserr("sigaction");exit(1);}
+	printf("Señal SIGALRM capturada\n");
+
+	sigprocmask(SIG_BLOCK,&mimask,&oldmask);	/* bloqueo SIGALRM */
+	printf("Señal SIGALRM bloqueada\n");
+
+/* region critica */
+	alarm(3);
+	printf("Alarma programada\n");
+	if (sigsuspend(&oldmask) != -1) syserr("sigsuspend");
+/* fin region critica */	
+	
+	printf("Fin programa\n");
+	
+	exit(0);
+}
+
+void captura(int n){
+	printf("Funcion de captura...%d\n",n);
+}
+
+
