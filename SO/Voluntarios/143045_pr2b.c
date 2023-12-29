4f5b02dc816bb779ae6923789aeb6341bf15087e
antoniocenlaces <antoniocenlaces@gmail.com>
2023-12-29 21:44:16 +0100

    Voluntario 2b para N hijos con un sleep(1)

--- SO/Voluntarios/143045_pr2b.c
+++ SO/Voluntarios/143045_pr2b.c
@@ -12,7 +12,7 @@
 #include <sys/wait.h>
 #include "error.h"
 
-#define MAL (void (*)(int)) -1
+
 #define N 2 //Número de procesos hijos que intercambian la señal SIGUSR1 con su padre
 
 /* N > 0 aunque no se recomienda N > 40 en Hendrix */
@@ -26,34 +26,35 @@ int main(){
       int i, myFather; // Contador y variable para almacenar el pid del padre en cada hijo
 
       sigset_t myMask,oldMask;
-	struct sigaction myAction;
+	    struct sigaction myAction1, myAction2, myAction3;
 
       // Programación de la captura de señales
       // Todo comportamiento ante señales programado en este proceso (padre) será heredado por sus hijos
       // SIGUSR1
-      myAction.sa_handler = capturaUSR1;
-      sigemptyset(&myAction.sa_mask);
-      i = sigaction(SIGUSR1, &myAction, NULL);
+      myAction1.sa_handler = capturaUSR1;
+      sigemptyset(&myAction1.sa_mask);
+      i = sigaction(SIGUSR1, &myAction1, NULL);
       if (i==-1) syserr("sigactionUSR1");
 
       // SIGUSR2
-      myAction.sa_handler = capturaUSR2;
-      i = sigaction(SIGUSR2, &myAction, NULL);
+      myAction2.sa_handler = capturaUSR2;
+      sigemptyset(&myAction2.sa_mask);
+      i = sigaction(SIGUSR2, &myAction2, NULL);
       if (i==-1) syserr("sigactionUSR2");
 
       // SIGINT
-      myAction.sa_handler = capturaINT;
-      i = sigaction(SIGINT, &myAction, NULL);
+      myAction3.sa_handler = capturaINT;
+      sigemptyset(&myAction3.sa_mask);
+      i = sigaction(SIGINT, &myAction3, NULL);
       if (i==-1) syserr("sigactionINT");
 
       sigemptyset(&myMask); // SET vacío
       sigemptyset(&oldMask);
       sigaddset(&myMask, SIGUSR1); // Añade SIGUSR1
       sigaddset(&myMask, SIGUSR2); // Añade SIGUSR2
-      // sigaddset(&myMask, SIGINT); // Añade SIGINT
 
-      // Bloqueamos las tres señales añadidas al SET
-      sigprocmask(SIG_BLOCK, &myMask, &oldMask);
+      // Bloqueamos las DOS señales añadidas al SET
+      sigprocmask(SIG_SETMASK, &myMask, &oldMask);
 
       // Bucle que lanza N procesos hijos, cada hijo se queda en pause al ser creado
       // El padre cuando los tiene todos creados los va despertando con la SIGUSR2 uno a uno
@@ -63,7 +64,7 @@ int main(){
             if ((pid[i] = fork()) == 0) { // Código para los hijos
                   myFather = getppid(); // pid de mi padre para poder enviarle la SIGUSR1
                   // sigsuspend() va a restaurar el SET anterior (elimina los bloqueos de la línea 56)
-                  // y después entra en pause, de que sale con la SIGUSR2 enviada por el padre
+                  // y después entra en pause, del que sale con la SIGUSR2 enviada por el padre
                   if (sigsuspend(&oldMask) != -1) syserr("sigsuspend");
                   // Al salir del sigsuspend() el SET myMask es restaurado => vuelven a estar bloqueadas
   
@@ -79,7 +80,7 @@ int main(){
       
       // Padre tiene localizados a los hijos en pid[]
       // Todo el código que sigue solo lo ejecuta el padre
-      
+      // sleep(1);
       // Bucle para despertar a los hijos. Inmediatamente entran en un sigsuspend()
       for (i=0; i < N; i++){
             kill(pid[i], SIGUSR2);
@@ -87,10 +88,11 @@ int main(){
       
       // en este momento los hijos están en pause() esperando que el padre decida quien empieza el juego
       i = 0;
+      sleep(1);
       while (i < 2147483647) {
             fprintf(stderr, "p");
             kill(pid[(i % N)], SIGUSR1);
-             if (sigsuspend(&oldMask) != -1) syserr("sigsuspend");
+            if (sigsuspend(&oldMask) != -1) syserr("sigsuspend");
             i++;
       }
       // No será fácil que llegue a ejecutarse el código siguiente, pero si alguien tiene tanta paciencia
@@ -107,16 +109,10 @@ int main(){
 } // main
 
 void capturaUSR1(int n){
-      void (*ff)(int);
-      ff = signal(SIGUSR1, capturaUSR1);
-      if (ff == MAL) syserr("signalUSR1");
       fprintf(stderr, "-");
 }
 
 void capturaUSR2(int n){
-      void (*ff)(int);
-      ff = signal(SIGUSR2, capturaUSR2);
-      if (ff == MAL) syserr("signalUSR2");
       fprintf(stderr, "\nAlguien ha enviado una SIGUSR2\n");
 }
 
