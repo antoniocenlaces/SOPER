f79cb70e4e388747b2965db06353aef5560afceb
antoniocenlaces <antoniocenlaces@gmail.com>
2023-12-31 16:41:23 +0000

    Voluntario 3 listo con memoria de procesos en juego

--- SO/Voluntarios/143045_pr3.c
+++ SO/Voluntarios/143045_pr3.c
@@ -15,16 +15,50 @@
 int senialado = 0;
 int termina = 0;
 
+// La llegada de la SIGUSR1 a cualquier proceso hace cambiar la variable global senialado
+// de ese proceso al valor 1.
 void capturaUSR1(int n);
+
+// Ante la señal SIGINT todos los procesos acaban de forma controlada
 void capturaINT(int n);
+
+// Pre: master es un FD abierto para lectura. Proporciona la comunicación de todos
+//      los procesos en el juego con informador (el proceso que ejecuta este procedimiento).
+// Post: Para cada mensaje que le llega por el FD master muestra una explicación por salida std
+// Nota: Codificación de mensajes. Se usa un vector de enteros con 3 componentes:
+//          - mensaje[0]: Codifica el tipo de mensaje que se ha recibido, siendo:
+//                          <0>: Un nuevo proceso se une al juego. Este proceso es descendiente del creador inicial
+//                          <1>: Un proceso (jugador) ha enviado la pelota de su vecino posterior
+//                          <2>: Un proceso (jugador) ha recibido la pelota de su vecino anterior
+//                          <3>: El proceso que inició el juego (padre inicial) se ha unido al juego
+//                          <4>: Un proceso (jugador) ha sido señalado por la SIGUSR1
+//          - mensaje[1]: pid del proceso (jugador) que envía el mensaje a informador
+//          - mensaje[2]: ppid del proceso que creó a este jugador / valor de la pelota
 void informador(int master);
+
+// Procedimiento que ejecutan todos los jugadores
+// Pre: padre es un entero con valor 1 si quien entra es el padre, 0 en caso contrario
+// Post: Bucle infinito donde:
+//       1.- Se espera a recibir la pelota del vecino anterior
+//       2.- Si ha llegado la señal SIGUSR1 a este proceso invoca a la rutina que duplica el proceso
+//       3.- Escribe a informador lo que ha recibido y lo que va a enviar
+//       4.- Envía la pelota al vecino posterior
 void entraEnJuego(int master, int padre);
+
+// Pre: master es un FD abierto para escritura. Proporciona la comunicación con el proceso
+//      informador.
+// Post: Duplica el proceso que invoca creaNuevoJugador() y establece un pipe de comunicación
+//       entre el proceso creador y el creado. Al acabar el proceso creador tiene en FD 1 un
+//       canal de escritura que le comunica con el nuevo proceso. El nuevo proceso tiene en
+//       FD 0 el canal donde leer de su padre y además heredó de su padre un canal (pipe)
+//       por donde puede comunicar (escribir) a su vecino posterior (que antes lo era de
+//       su padre)
 void creaNuevoJugador(int master);
 
 int main(){
     int i, pid;
 
-    sigset_t myMask,oldMask;
+    sigset_t myMask;
 	struct sigaction myAction1, myAction2;
 
     // Programación de la captura de señales
@@ -41,7 +75,7 @@ int main(){
     i = sigaction(SIGINT, &myAction2, NULL);
     if (i==-1) syserr("sigactionINT");
 
-    // Va a sre creado un proceso informador que relatará todo lo que va ocurriendo en el juego
+    // Va a ser creado un proceso informador que relatará todo lo que va ocurriendo en el juego
     // Todos los procesos tendrán el canal de escritura master para comunicar con el informador
     // Creamos un pipe asociado a master[2] que se heredará en todos los procesos a partir de ahora
     int master[2];
@@ -94,6 +128,7 @@ int main(){
             // Proceso de comunicación con el primer vecino de juego establecido
             // Ahora inicio enviándole la primera pelota
             pelota = 1;
+            // El juego se inicia con el envío de esta primera pelota
             write(1, &pelota, 4);
             entraEnJuego(master[1], 1); // El padre entra en el juego
     }
@@ -112,30 +147,38 @@ void entraEnJuego(int master, int padre){
         else mensaje[0] = 0;
     mensaje[1] = getpid();
     mensaje[2] = getppid();
-    fprintf(stderr, "Alguien acaba de entrar en juego getpid(): %d y su getppid(): %d\n", mensaje[1], mensaje[2]);
     write(master, mensaje, 12);
     int recibe, pelota;
     while (1) {
-        recibe = read(0, &pelota, 4);
+        recibe = read(0, &pelota, 4); // Me bloqueo hasta que me llegue la pelota
         if (recibe == 0) syserr("read");
-        mensaje[0] = 2;
+        // Una pelota ha llegado. Envío mensaje a informador
+        mensaje[0] = 2; // Pelota recibida
         mensaje[2] = pelota;
         write(master, mensaje, 12);
-        // He recibido la pelota y voy a comprobar si he de duplicarme para crear un nuevo hijo
-        if (senialado){
+        // Voy a comprobar si he de duplicarme para crear un nuevo hijo
+        if (senialado){ // senialado es varible global que cambia a 1 en la rutina de captura de SIGUSR1
             senialado = 0;
-            fprintf(stderr, "Proceso %d ha sido señalado\n", getpid());
+            mensaje[0] = 4; // Proceso señalado
+            write(master, mensaje, 12);
             creaNuevoJugador(master);
         }
-        pelota++;
-        mensaje[0] = 1;
+        pelota++; // Preparo pelota para mi vecino postarior
+        mensaje[0] = 1; // Pelota enviada
         mensaje[2] = pelota;
-        sleep(1);
+        sleep(1); // Espero 1s para enviar pelota
         write(master, mensaje, 12);
         write(1, &pelota, 4);
     }
 }
-
+// Pre: master es un FD abierto para escritura. Proporciona la comunicación con el proceso
+//      informador.
+// Post: Duplica el proceso que invoca creaNuevoJugador() y establece un pipe de comunicación
+//       entre el proceso creador y el creado. Al acabar el proceso creador tiene en FD 1 un
+//       canal de escritura que le comunica con el nuevo proceso. El nuevo proceso tiene en
+//       FD 0 el canal donde leer de su padre y además heredó de su padre un canal (pipe)
+//       por donde puede comunicar (escribir) a su vecino posterior (que antes lo era de
+//       su padre)
 void creaNuevoJugador(int master){
     // Soy el padre que se duplica para crear un nuevo jugador
     int nd[2];
@@ -157,7 +200,18 @@ void creaNuevoJugador(int master){
         close(nd[1]);
     }
 }
-
+// Pre: master es un FD abierto para lectura. Proporciona la comunicación de todos
+//      los procesos en el juego con informador (el proceso que ejecuta este procedimiento).
+// Post: Para cada mensaje que le llega por el FD master muestra una explicación por salida std
+// Nota: Codificación de mensajes. Se usa un vector de enteros con 3 componentes:
+//          - mensaje[0]: Codifica el tipo de mensaje que se ha recibido, siendo:
+//                          <0>: Un nuevo proceso se une al juego. Este proceso es descendiente del creador inicial
+//                          <1>: Un proceso (jugador) ha enviado la pelota de su vecino posterior
+//                          <2>: Un proceso (jugador) ha recibido la pelota de su vecino anterior
+//                          <3>: El proceso que inició el juego (padre inicial) se ha unido al juego
+//                          <4>: Un proceso (jugador) ha sido señalado por la SIGUSR1
+//          - mensaje[1]: pid del proceso (jugador) que envía el mensaje a informador
+//          - mensaje[2]: ppid del proceso que creó a este jugador / valor de la pelota
 void informador(int master){
     int recibe, mensaje[3], jugadores[50], n=0;
     while (1){
@@ -179,14 +233,20 @@ void informador(int master){
                 jugadores[n] = mensaje[1];
                 n++;
                 printf("Jugador Padre: %d con pid=%d, creado por: %d\n",n , mensaje[1], mensaje[2]);
+                break;
+            case 4:
+                printf("Jugador pid=%d, ha sido señalado por SIGUSR1\n", mensaje[1]);
         }
     }
 }
 
+// La llegada de la SIGUSR1 a cualquier proceso hace cambiar la variable global senialado
+// de ese proceso al valor 1.
 void capturaUSR1(int n){
     senialado = 1;
 }
 
+// Ante la señal SIGINT todos los procesos acaban de forma controlada
 void capturaINT(int n){
       fprintf(stderr, "\nHe recibido SIGINT\n");
       exit(0);
