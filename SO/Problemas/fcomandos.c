95c1bb1e599a7f3dc968088f2d443b84d217dee5
antoniocenlaces <antoniocenlaces@gmail.com>
2024-01-04 17:11:07 +0000

    fcomandos de nuevo con comentarios

--- SO/Problemas/fcomandos.c
+++ SO/Problemas/fcomandos.c
@@ -127,9 +127,9 @@ fprintf(stderr,"Iniciado ejecuta para el comando: %s | %s\n",arg1[0], arg2[0]);
             close(fd[0]);
             close(fd[1]);
             i = wait(estado);
-            fprintf(stderr,"Mi hijo %d ha terminado correctamente con estado: %d\n",i, estado);
+            fprintf(stderr,"Mi hijo %d ha terminado correctamente con estado: %d\n",i, *estado);
             i = wait(estado);
-            fprintf(stderr,"Mi hijo %d ha terminado correctamente con estado: %d\n",i, estado);
+            fprintf(stderr,"Mi hijo %d ha terminado correctamente con estado: %d\n",i, *estado);
         } // switch2
     } // switch1
 } // ejecuta
